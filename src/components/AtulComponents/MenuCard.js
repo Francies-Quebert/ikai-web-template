8dbe0bdcec148278f1cc98600b7a0b7030867185
hariom shah <hariom23shah@gmail.com>
2021-05-10 16:15:38 +0530

    atul

---
+++ src/components/AtulComponents/MenuCard.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const MenuCard = props => {
+  return (
+    <>
+      <div className="h-auto ...">
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </>
+  )
+}
+
+export default MenuCard
---
+++ src/components/AtulComponents/Reservation.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Lines from "../../images/lines.png"
+import ResLogo from "../../images/reservations_logo.png"
+
+const Reservation = () => {
+  return (
+    <>
+      <div className="p-3.5">
+        <div>
+          <h2 className="reservation-title">Reservations</h2>
+        </div>
+        <div className="flex justify-center">
+          <p
+            className="text-center text-base mb-12 font-normal max-w-xl"
+            style={{ color: "#777777" }}
+          >
+            Booking a table has never been so easy with free & instant online
+            restaurant reservations, booking now!!
+          </p>
+        </div>
+        <div className="">
+          <div className="block flex justify-center">
+            <img src={ResLogo} />
+          </div>{" "}
+          <div
+            style={{ color: "#4d4c4a" }}
+            className="flex justify-center text-5xl uppercase font-bold"
+          >
+            Make a reservation
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Reservation
