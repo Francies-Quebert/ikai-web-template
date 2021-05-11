e625b80d12b06089fd72326f2c55f95e103e2692
Govind Saini <quebert88@gmail.com>
2021-05-11 18:01:25 +0530

    govind

--- src/components/New-govind/Homepage/Footer.js
+++ src/components/New-govind/Homepage/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 md:gap-8 lg:grid-cols-3 bg-gray-300 px-10 mt-5 py-10">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 md:gap-8 lg:grid-cols-3 bg-gray-300 px-10 mt-5 text-center py-10">
         <div>
           <h1 className="tracking-tighter">
             Copyright © 2021 <span className="text-red-500">Resturants.</span>{" "}
@@ -16,11 +16,11 @@ const Footer = () => {
             570 8th Ave, New York, NY 10018 United States
           </h1>
         </div>
-        <div className="flex hover:text-red-600 space-x-4">
-          <Link to="/about">
+        <div className="flex text-center mx-auto space-x-4">
+          <Link to="/about " className="hover:text-red-600" >
             <p>About Us</p>
           </Link>
-          <Link to="/contact">
+          <Link to="/contact" className="hover:text-red-600">
             <p>Contact Us</p>
           </Link>
         </div>
