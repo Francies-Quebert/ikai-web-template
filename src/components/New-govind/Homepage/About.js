67d566d6dc96269e9d6dfdd84d9f25fc6e59377d
Govind Saini <quebert88@gmail.com>
2021-05-11 11:55:33 +0530

    govind

--- src/components/New-govind/Homepage/About.js
+++ src/components/New-govind/Homepage/About.js
@@ -2,6 +2,7 @@ import { any } from "prop-types"
 import React from "react"
 import AB_IMG from "../../../images/ab_1.jpg"
 import AB_S1 from "../../../images/ab_s1.jpg"
+import AB_HDR1 from "../../../images/header_decorator.png"
 
 const About = () => {
   const abData = {
@@ -9,34 +10,34 @@ const About = () => {
     content:
       "We love restaurants as much as you do. That’s why we’ve been helping them fill tables since 1999. Welcome to elixir restaurant",
     text: " The History",
-    content1: `" The
-      <span className="text-red-600 font-bold">History of Kitchens</span>
-      and Cooks gives further intimation on Mr Boulanger usual menu,
-      stating confidently that "Boulanger served salted poultry and fresh
+    content1: ` The History of Kitchens and Cooks gives further intimation
+      on Mr Boulanger usual menu, stating confidently that "Boulanger served 
+      salted poultry and fresh
       eggs, all presented without a tablecloth on small marble tables".
       Numerous commentators have also referred to the supposed restaurant
       owner's eccentric habit of touting for custom outside his
       establishment, dressed in aristocratic fashion and brandishing a
-      sword"`,
-    content2: `" According to Miss Spang, there is not a shred of evidence for any of
+      sword`,
+    content2: ` According to Miss Spang, there is not a shred of evidence for any of
       it. She said: These legends just get passed on by hearsay and then
-      spiral out of control. Her interest in
-      <span className="text-red-600 font-bold">Boulanger</span> dates back 
-      to a history of food seminar in Paris in the mid-1990s"`,
+      spiral out of control. Her interest in Boulanger dates back 
+      to a history of food seminar in Paris in the mid-1990s`,
   }
 
   return (
-    <section className="mt-20 px-10">
-      <div className="text-center">
+    <section className="mt-5 px-10">
+      <div className="text-center ">
+        <img src={AB_HDR1} className=" ml-auto -mb-2 mr-96" />
+        <img src={AB_HDR1} className="ml-96" />
+
         <h1 className="ab-title ">{abData.title}</h1>
-        <p className="ab-content">{abData.content}</p>
+
+        <p className="ab-content ">{abData.content}</p>
       </div>
 
       <div className=" ab-div">
         <div>
-          <h1 className="ab-text">
-            {abData.text}
-          </h1>
+          <h1 className="ab-text">{abData.text}</h1>
           <p className="ab-content1">{abData.content1}</p>
           <p className="ab-content1">{abData.content2}</p>
         </div>
--- src/components/New-govind/Homepage/OpeningHour.js
+++ src/components/New-govind/Homepage/OpeningHour.js
@@ -1,5 +1,8 @@
 import React from "react"
 import MENUBG from "../../../images/opening_hours.jpg"
+import AB_DDR1 from "../../../images/dec1.png"
+import AB_DDR2 from "../../../images/dec2.png"
+import AB_DDR3 from "../../../images/column-divider.png"
 
 const OpeningHour = () => {
   const opHData = {
@@ -16,7 +19,9 @@ const OpeningHour = () => {
       <div className="oph-body ">
         <img src={MENUBG} className="oph-img" />
         <h1 className="oph-header">{opHData.header}</h1>
-        <p className="oph-text ">{opHData.text}</p>
+        <img src={AB_DDR1} alt="" className="relative -mt-16  mx-40" /> 
+        <img src={AB_DDR2} alt="" className="relative ml-auto mr-175 -mt-138  " />
+        <p className="italic text-red-600 text-center text-xl -mt-14 relative ">{opHData.text}</p>
 
         <div className="oph-div">
           <div>
@@ -28,6 +33,7 @@ const OpeningHour = () => {
             <p className="oph-day2">{opHData.time2}</p>
           </div>
         </div>
+        <img src={AB_DDR3} alt="" />
         <p className="oph-content ">{opHData.content}</p>
       </div>
     </>
