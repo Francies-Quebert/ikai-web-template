fb5ca81fdab96df5745e119f41c7ff3b562f609d
hariom shah <hariom23shah@gmail.com>
2021-05-12 14:56:18 +0530

    saurav footer

--- src/pages/saurav.js
+++ src/pages/saurav.js
@@ -1,12 +1,14 @@
 import React from "react"
 import MenuPage from "../components/SauravComponents/Menu/MenuPage"
 import ContactUs from "../components/SauravComponents/ContactUs/ContactUs"
+import Footer from "../components/SauravComponents/Footer/footer"
 
 const saurav = () => {
   return (
     <>
       <MenuPage />
       <ContactUs />
+      <Footer/>
     </>
   )
 }
